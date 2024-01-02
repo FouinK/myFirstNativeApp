1b8fa4e67d21a12dcc00258f0f08604d820d4120
Roy <yyss9103@gmail.com>
2024-01-02 22:16:22 +0900

    일단 메뉴 선택인데 영역이 너무 커졌다

--- component/TopMenu.tsx
+++ component/TopMenu.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {Text, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
 
 const TopMenu = () => {
+  const [selectedMenu, setSelectedMenu] = useState('menu1');
+  const handleSelectMenu = (menu: string) => {
+    // 상태를 업데이트합니다.
+    setSelectedMenu(menu);
+  };
+
   return (
-    <View style={styles.title}>
-      <Text>며칠째인지 나오게</Text>
-    </View>
+    <ScrollView
+      horizontal={true}
+      style={styles.scrollView}
+      showsHorizontalScrollIndicator={false}>
+      {/* 각 메뉴 항목을 반복해서 렌더링합니다. */}
+      <TouchableOpacity
+        style={[styles.menuItem, selectedMenu === '1' && styles.selectedMenu]}
+        onPress={() => handleSelectMenu('1')}>
+        <Text>메뉴 1</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.menuItem, selectedMenu === '2' && styles.selectedMenu]}
+        onPress={() => handleSelectMenu('2')}>
+        <Text>메뉴 2</Text>
+      </TouchableOpacity>
+    </ScrollView>
   );
 };
 
 export default TopMenu;
 
 const styles = StyleSheet.create({
-  title: {
-    flex: 0.1,
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
+  scrollView: {
     backgroundColor: 'white',
-    borderBottomWidth: 1, // 구분선의 두께
-    borderBottomColor: '#dedede', // 구분선의 색상
+    borderBottomWidth: 1,
+    borderBottomColor: '#dedede',
+    flex: 0.1,
+  },
+  menuItem: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+  },
+  selectedMenu: {
+    borderBottomWidth: 3,
+    borderBottomColor: 'blue',
   },
 });
