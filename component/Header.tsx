88845bdbbf3d2264238866d1e1d2ee406b41efa0
Roy <yyss9103@gmail.com>
2024-01-02 21:47:16 +0900

    feat : 헤더에 이름이랑 아이콘 css 작업

--- component/Header.tsx
+++ component/Header.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
 
 const Header = () => {
@@ -8,34 +14,34 @@ const Header = () => {
   };
 
   return (
-    <View style={styles.header}>
-      <Text style={styles.title}>어플 이름</Text>
-      <TouchableOpacity onPress={openSettings} style={styles.settingsButton}>
-        <Icon name="delete" size={25} color="#000" />
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.header}>
+        <Text style={styles.title}>어플 이름</Text>
+        <TouchableOpacity onPress={openSettings} style={styles.settingsButton}>
+          <Icon name="delete" size={25} color="#000" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
-export default Header;
-
 const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: 'white',
+  },
   header: {
     width: '100%',
-    height: '10%',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
     backgroundColor: 'white',
-    borderBottomWidth: 1, // 구분선의 두께
-    borderBottomColor: '#dedede', // 구분선의 색상
-    flexDirection: 'row',
-    padding: 1,
+    borderBottomWidth: 1,
+    borderBottomColor: '#dedede',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
   },
-  title: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  settingsButton: {
-    // 설정 버튼에 대한 스타일
-  },
+  title: {},
+  settingsButton: {},
 });
+
+export default Header;
