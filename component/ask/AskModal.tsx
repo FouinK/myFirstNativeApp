39141d1eb711d809d53492bab70fb427fb74bd98
Roy <yyss9103@gmail.com>
2024-01-06 00:15:03 +0900

    모달 좀 예쁘게 추가

--- component/ask/AskModal.tsx
+++ component/ask/AskModal.tsx
@@ -10,7 +10,7 @@ const AskModal: React.FC<AskModalProps> = ({visible, setVisible}) => {
   return (
     <Modal
       animationType="slide"
-      transparent={true} // 투명 배경 설정
+      transparent={true}
       visible={visible}
       onRequestClose={() => setVisible(false)}>
       <View style={styles.centeredView}>
@@ -20,7 +20,14 @@ const AskModal: React.FC<AskModalProps> = ({visible, setVisible}) => {
             style={styles.textInput}
             placeholder="여기에 질문을 작성하세요"
           />
-          <Button title="제출하기" onPress={() => setVisible(false)} />
+          <View style={styles.buttonContainer}>
+            <Button title="제출하기" onPress={() => setVisible(false)} />
+            <Button
+              title="취소"
+              color="red"
+              onPress={() => setVisible(false)}
+            />
+          </View>
         </View>
       </View>
     </Modal>
@@ -30,16 +37,15 @@ const AskModal: React.FC<AskModalProps> = ({visible, setVisible}) => {
 const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
+    justifyContent: 'center', // 세로 방향으로 중앙 정렬
+    alignItems: 'center', // 가로 방향으로 중앙 정렬
   },
   modalView: {
-    margin: 20,
+    width: '80%', // 화면 너비의 80%
     backgroundColor: 'white',
     borderRadius: 20,
     padding: 35,
-    alignItems: 'center',
+    alignItems: 'center', // 내부 요소를 가로 방향으로 중앙 정렬
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
@@ -49,12 +55,18 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5,
   },
+  buttonContainer: {
+    flexDirection: 'row-reverse', // 요소들을 역순으로 배치하여 '취소' 버튼이 오른쪽 끝에 위치하도록 함
+    justifyContent: 'flex-start', // 요소들이 컨테이너의 시작 부분(여기서는 오른쪽 끝)에 위치하도록 함
+    width: '100%', // 버튼 컨테이너의 너비를 modalView의 너비와 동일하게 설정
+    marginTop: 20,
+  },
   textInput: {
     height: 40,
     marginVertical: 10,
     borderWidth: 1,
     padding: 10,
-    width: '100%',
+    width: '100%', // 텍스트 입력 필드의 너비를 modalView의 너비와 동일하게 설정
   },
 });
 
